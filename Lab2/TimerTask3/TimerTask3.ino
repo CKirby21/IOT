19c502aa88d6d0e4d42a70143cae118e890d3f20
Caden Kirby <cadenkirby@outlook.com>
2022-09-07 18:03:38 -0500

    finished Task3

--- Lab2/TimerTask3/TimerTask3.ino
+++ Lab2/TimerTask3/TimerTask3.ino
@@ -2,83 +2,68 @@
 // D13 (PIN_LED_13): Blue
 // TX (PIN_LED_TXL): Green
 // RX (PIN_LED_RXL): Yellow
-#define CPU_HZ 48000000
-// TODO Change this
-#define TIMER_PRESCALER_DIV 1024
-void startTimer(int frequencyHz);
-void setTimerFrequency(TcCount8* TC, int frequencyHz);
-void TC3_Handler();
+
 bool isBlueOn = false;
-//bool isYellowOn = false;
+bool isYellowOn = false;
 int interruptCount = 0;
 
 void setup() {
- SerialUSB.begin(9600);
- // while(!SerialUSB);
- pinMode(PIN_LED_13, OUTPUT);
-// pinMode(PIN_LED_RXL, OUTPUT);
-// digitalWrite(PIN_LED_RXL, HIGH);
- startTimer(1);
-}
-void loop() {}
-
-void setTimerFrequency(TcCount8* TC, int frequencyHz) {
   
-  // TODO Write to 8-bit counter's Period register (PER) to control the top value
-  int periodValue = (CPU_HZ / (TIMER_PRESCALER_DIV * frequencyHz)) - 1;
-
-  // TODO Something like this?
-  TC->PER.reg = periodValue; 
-  // Make sure the count is in a proportional position to where it was
-  // to prevent any jitter or disconnect when changing the compare value.
-
-  // TODO Not sure I need this
-  // TC->COUNT.reg = map(TC->COUNT.reg, 0, TC->CC[0].reg, 0, compareValue);
-  SerialUSB.print("TC Count register: ");
-  SerialUSB.println(TC->COUNT.reg);
-  SerialUSB.print("TC Period register: ");
-  SerialUSB.println(TC->PER.reg);
-  while (TC->STATUS.bit.SYNCBUSY == 1);
-}
-
-void startTimer(int frequencyHz) {
-  REG_GCLK_CLKCTRL = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID_TCC2_TC3) ;
-  while ( GCLK->STATUS.bit.SYNCBUSY == 1 ); // wait for sync
-  TcCount8* TC = (TcCount8*) TC3;
-
-  TC->CTRLA.reg &= ~TC_CTRLA_ENABLE; //Disable timer
-  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
-  // Use the 8-bit timer
-  TC->CTRLA.reg |= TC_CTRLA_MODE_COUNT8;
-  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
-
-  // TODO Use Normal Mode (NFRQ)
-  // When MFRQ is used, the value in CC0 will be used as the top value and WO[0] will toggle on every overflow/underflow.
-  // When NFRQ is used, the waveform output (WO[x]) toggles every time CCx and the counter are equal, and the interrupt flag corresponding to that channel will be set.
+  pinMode(PIN_LED_13, OUTPUT);
+  pinMode(PIN_LED_RXL, OUTPUT);
+  digitalWrite(PIN_LED_RXL, HIGH);
   
-  // TODO Change this
-  TC->CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1024;
-  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+   // Set up the generic clock (GCLK2) used to clock timers
+  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(4) |          // Divide the 48MHz clock source by divisor 3: 32zKHz/(2^(4+1))=1KHz
+                    GCLK_GENDIV_ID(2);            // Select Generic Clock (GCLK) 2
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
 
-  setTimerFrequency(TC, frequencyHz);
-  
-  // Enable the compare interrupt
-  TC->INTENSET.reg = 0; // This clears the entire register?
-  TC->INTENSET.bit.OVF = 1; 
-  NVIC_EnableIRQ(TC3_IRQn);
-  TC->CTRLA.reg |= TC_CTRLA_ENABLE;
-  while (TC->STATUS.bit.SYNCBUSY == 1); // wait for sync
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(2) |        // Select GCLK2
+                    GCLK_GENCTRL_GENEN |          // Enable GCLK2
+                    GCLK_GENCTRL_SRC_OSCULP32K |  // Set the 32KHz clock source
+                    GCLK_GENCTRL_DIVSEL;
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+  // Feed GCLK2 to TC3 and TC2
+  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TC3 and TC2
+                     GCLK_CLKCTRL_GEN_GCLK2 |     // Select GCLK4
+                     GCLK_CLKCTRL_ID_TCC2_TC3;     // Feed the GCLK4 to TC3 and TC2
+  while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
+
+  REG_TC3_CTRLA |= TC_CTRLA_MODE_COUNT8;           // Set the counter to 8-bit mode
+  while (TC3->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+
+  REG_TC3_COUNT8_PER = 0xFF;                      // Set the PER (period) register to its maximum value
+  while (TC3->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
+
+  NVIC_SetPriority(TC3_IRQn, 0);    // Set the Nested Vector Interrupt Controller (NVIC) priority for TC3 to 0 (highest)
+  NVIC_EnableIRQ(TC3_IRQn);         // Connect TC3 to Nested Vector Interrupt Controller (NVIC)
+
+  REG_TC3_INTFLAG |= TC_INTFLAG_OVF;        // Clear the interrupt flags
+  REG_TC3_INTENSET = TC_INTENSET_OVF;     // Enable TC3 interrupts
+ 
+  REG_TC3_CTRLA |= TC_CTRLA_PRESCALER_DIV2 |     // Set prescaler to 2, 1KHz/2 = 512Hz
+                   TC_CTRLA_ENABLE;               // Enable TC3
+  while (TC3->COUNT8.STATUS.bit.SYNCBUSY);        // Wait for synchronization
 }
 
-// TODO Need to use the overflow interrupt instread of the match interrupt
-void TC3_Handler() {
-  TcCount8* TC = (TcCount8*) TC3;
-  // If this interrupt is due to the compare register matching the timer count
-  // we toggle the LED.
-  if (TC->INTFLAG.bit.OVF == 1) {
-    TC->INTFLAG.bit.OVF = 1;
-    // Write callback here!!!
-    digitalWrite(PIN_LED_13, isBlueOn);
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void TC3_Handler()
+{     
+  // Check for overflow (OVF) interrupt
+  if (TC3->COUNT8.INTFLAG.bit.OVF)             
+  {
     isBlueOn = !isBlueOn;
+    digitalWrite(PIN_LED_13, isBlueOn);
+    if (interruptCount % 2 == 0) {
+      digitalWrite(PIN_LED_RXL, isYellowOn);
+      isYellowOn = !isYellowOn;
+    }
+    interruptCount++;
+    REG_TC3_INTFLAG = TC_INTFLAG_OVF;         // Clear the OVF interrupt flag
   }
 }
