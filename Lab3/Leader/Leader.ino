ae88b9e6d9489b9930840e2ab5abcd401f595ae3
Caden <88149251+CKirby21@users.noreply.github.com>
2022-09-15 19:36:39 -0500

    pretty up the leader

--- Lab3/Leader/Leader.ino
+++ Lab3/Leader/Leader.ino
@@ -20,8 +20,6 @@ RH_RF95 rf95(12, 6);
 // Reserve a portion of flash memory to store an "int" variable
 // and call it "my_flash_store".
 FlashStorage(my_flash_store, int);
-// Note: the area of flash memory reserved for the variable is
-// lost every time the sketch is uploaded on the board.
 
 int LED = 13; //Status LED is on pin 13
 int packetCounter = 0; //Counts the number of packets sent
@@ -40,77 +38,73 @@ void setup()
   pinMode(LED, OUTPUT);
   SerialUSB.begin(9600);
   // while (!SerialUSB);
-
-  SerialUSB.println("RFM Client!");
+  
   //Initialize the Radio.
+  SerialUSB.println("RFM Client!");
   if (rf95.init() == false) {
     SerialUSB.println("Radio Init Failed - Freezing");
-  while (1);
+    while (1);
   }
   else {
-  //An LED inidicator to let us know radio initialization has completed.
-  SerialUSB.println("Receiver up!");
-  digitalWrite(LED, HIGH);
-  delay(500);
-  digitalWrite(LED, LOW);
-  delay(500);
+    //An LED inidicator to let us know radio initialization has completed.
+    SerialUSB.println("Receiver up!");
+    digitalWrite(LED, HIGH);
+    delay(500);
+    digitalWrite(LED, LOW);
+    delay(500);
   }
-  // Set frequency
   rf95.setFrequency(frequency);
   // Transmitter power can range from 14-20dbm.
   rf95.setTxPower(20, false);
   uint8_t toSend[] = "Start";  
   rf95.send(toSend, sizeof(toSend));
   rf95.waitPacketSent();  
-
   rf95.setTxPower(20, true);
-  
 }
 
 
 void loop()
 {
   if (rf95.available()) {
-    // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len)){
-    digitalWrite(LED, HIGH); //Turn on status LED
-    timeSinceLastPacket = millis(); //Timestamp this packet
-    SerialUSB.println((char*)buf);
-
-    char *message = strtok((char *)buf, ",");
-    int nodeID = atoi(message);
-
-    message = strtok(NULL, ",");
-    int packetID = atoi(message);
-
-    message = strtok(NULL, ",");
-    unsigned long timestamp = strtoul(message, NULL, 10);
-
-    message = strtok(NULL, ",");
-    float temperature = atof(message);
-
-    message = strtok(NULL, ",");
-    int errorCode = atoi(message);
-
-    int idealPacketID = lastPacketIDs[nodeID] + 1;
-    if (packetID > idealPacketID)
-      errorCode = 2; 
-    lastPacketIDs[nodeID] = packetID;
-      
-    // Print error code
-    if (errorCode > 0) {
-      my_flash_store.write(errorCode);
-      SerialUSB.print("Node ");
-      SerialUSB.print(nodeID);
-      SerialUSB.print(" had an error code of ");
-      SerialUSB.print(my_flash_store.read());
-      SerialUSB.print(" at time ");
-      SerialUSB.println(timestamp);
-    }
-  } else
-    SerialUSB.println("Recieve failed");
+      digitalWrite(LED, HIGH); //Turn on status LED
+      timeSinceLastPacket = millis(); //Timestamp this packet
+      SerialUSB.println((char*)buf);
+  
+      char *message = strtok((char *)buf, ",");
+      int nodeID = atoi(message);
+  
+      message = strtok(NULL, ",");
+      int packetID = atoi(message);
+  
+      message = strtok(NULL, ",");
+      unsigned long timestamp = strtoul(message, NULL, 10);
+  
+      message = strtok(NULL, ",");
+      float temperature = atof(message);
+  
+      message = strtok(NULL, ",");
+      int errorCode = atoi(message);
+  
+      int idealPacketID = lastPacketIDs[nodeID] + 1;
+      if (packetID > idealPacketID)
+        errorCode = 2; 
+      lastPacketIDs[nodeID] = packetID;
+        
+      // Log error and write it to flash
+      if (errorCode > 0) {
+        my_flash_store.write(errorCode);
+        SerialUSB.print("Node ");
+        SerialUSB.print(nodeID);
+        SerialUSB.print(" had an error code of ");
+        SerialUSB.print(my_flash_store.read());
+        SerialUSB.print(" at time ");
+        SerialUSB.println(timestamp);
+      }
+    } else
+      SerialUSB.println("Recieve failed");
   }
   //Turn off status LED if we haven't received a packet after 1s
   if(millis() - timeSinceLastPacket > 1000){
