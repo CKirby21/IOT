2f83231fd917c02cb996179879b3583f1b819a37
Cody Binder <ctbind01@gmail.com>
2022-11-13 14:28:20 -0600

    Created dummy frontend for login and register

--- Fetch/Fetch/src/pages/login/login.tsx
+++ Fetch/Fetch/src/pages/login/login.tsx
@@ -1,18 +1,46 @@
-import './login.css';
-import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import "./login.css";
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+  IonCheckbox,
+} from "@ionic/react";
 import Header from "../../components/header/header";
 
 const Login: React.FC = () => {
-  return(
+  return (
     <IonPage>
       <IonContent>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
         <Header />
+        <form className="ion-padding">
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput type="email" required />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput type="password" required />
+          </IonItem>
+          <IonItem lines="none">
+            <IonLabel>Remember me</IonLabel>
+            <IonCheckbox defaultChecked={true} slot="start" />
+          </IonItem>
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            Login
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
--- Fetch/Fetch/src/pages/register/register.tsx
+++ Fetch/Fetch/src/pages/register/register.tsx
@@ -1,18 +1,45 @@
-import './register.css';
-import React from 'react';
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import "./register.css";
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+} from "@ionic/react";
 import Header from "../../components/header/header";
 
 const Register: React.FC = () => {
-  return(
+  return (
     <IonPage>
       <IonContent>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Register</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Register</IonTitle>
+          </IonToolbar>
+        </IonHeader>
         <Header />
+        <form className="ion-padding">
+          <IonItem>
+            <IonLabel position="floating">Username</IonLabel>
+            <IonInput required />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput type="email" required />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="floating">Password</IonLabel>
+            <IonInput type="password" required />
+          </IonItem>
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            Sign Up
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
